1d6b83769e883723c832e078f390fd6d030695de
Blessing <blessingshongwe@gmail.com>
2021-10-01 13:24:15 +0200

    added emailing service

--- main/settings.py
+++ main/settings.py
@@ -70,6 +70,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- newletter/models.py
+++ newletter/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Emails(models.Model):
@@ -10,3 +13,23 @@ class Emails(models.Model):
 
     def __str__(self):
         return self.email
+
+
+@receiver(post_save, sender=Emails)
+def send_new_email(sender, instance, created, **kwargs):
+
+    # If new subscriber is added
+    if created:
+        email = instance.email if instance.email else "no email given"
+
+        subject = "Welcome to Prime Time Code"
+        message = 'Hi! Welcome  to Prime Time Code. '
+
+        send_mail(
+            subject,
+            message,
+            'blessing@primetimecode.co.za',
+            [email],
+
+            fail_silently=False,
+        )
