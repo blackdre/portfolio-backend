6c6e6f33b6169b8a113eb351bb52a434c70defd3
Blessing <blessingshongwe@gmail.com>
2021-10-01 19:49:42 +0200

    created contact app

---
+++ contact/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
---
+++ contact/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
